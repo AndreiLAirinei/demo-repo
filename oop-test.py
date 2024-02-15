cccfd7517efe9572183a72237654d018f38380f7
Andrei Airinei <airinei.andrei.laurentiu@gmail.com>
2024-02-15 09:18:13 +0200

    Adding csv file and importing from it

--- oop-test.py
+++ oop-test.py
@@ -1,6 +1,8 @@
+import csv
 
 class Item:
     pay_rate = 0.8  # The pay rate after 20% discount
+    all = []
 
     def __init__(self, name: str, price: float, quantity=0):
         # Run validations to the received arguments
@@ -13,18 +15,39 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        # Actions to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
 
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open ('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
 
-item1 = Item("Phone", 100, 1)
-item2 = Item("Laptop", 1000, 3)
-item3 = Item("Cable", 10, 5)
-item4 = Item("Mouse", 50, 5)
-item5 = Item("Keyboard", 75, 5)
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity')),
+            )
+
+    def __repr__(self):
+        return f"Item('{self.name}',{self.price}, {self.quantity})"
+
+
+Item.instantiate_from_csv()
+print(Item.all)
+
+
+# Separating every instance by an attribute, using a for loop
+# for instance in Item.all:
+#    print(instance.name)
 
 
 # Example of modifying the discount after creating the instance
