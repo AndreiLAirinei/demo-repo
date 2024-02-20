2f60e15c0b33b82ca65765f722bb179c51102216
Andrei Airinei <airinei.andrei.laurentiu@gmail.com>
2024-02-20 19:09:30 +0200

    abstruction example through an email

--- item.py
+++ item.py
@@ -80,3 +80,23 @@ class Item:
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}',{self.price}, {self.quantity})"
+
+    def __connect(self, smtp_server):
+        pass
+
+    def __prepare_body(self):
+        return f"""
+        Hello someone,
+        
+        We have {self.name} {self.quantity} times.
+        Regards, Mr. seller 
+    """
+
+    def __send(self):
+        pass
+
+    def send_email(self):
+        self.__connect('')
+        self.__prepare_body()
+        self.__send()
+        print("Email sent!")
--- main-oop.py
+++ main-oop.py
@@ -12,9 +12,11 @@ print(item1.name)
 # Example of encapsulation
 item2 = Item("2ndItem", 800)
 
+print(item2)
 item2.apply_increment(0.1)
 
-print(item2)
+item2.send_email()
+
 
 if __name__ == '__main__':
     pass
